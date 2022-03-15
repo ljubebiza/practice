fb364426ec608947e23d8d6a28fe2b49d100e28b
ljubebiza <srbija.majka@gmail.com>
2022-03-15 23:40:00 +0100

    second change

--- stopWatch/sw.js
+++ stopWatch/sw.js
@@ -3,19 +3,45 @@ var stopTime = true;
 var sec = 0;
 var  min = 0;
 var  hour = 0;
+var time;
+
+function counting(){
+    if(stopTime != true){
+           sec++
+       
+   
+    if(sec == 60){
+        sec = 0
+        min++
+       }
+   
+    if(min == 60){
+        min = 0;
+        sec = 0;
+        hour++
+       }
+   
+       if(sec < 10){sec = '0' + sec}
+       if(min < 10){min = '0' + min}
+       if(hour < 10){hour = '0' + hour}
+   
+    sw.innerHTML = hour.slice(-2) + ":" +  min.slice(-2) + ":" +  sec;
+    time = setTimeout("counting()", 1000);
+                   
+     }
+   }
 
 function startSw(){
-
- if(stopTime === true){
    stopTime = false;
-   counting()
-   }
+   counting();
+   
 }
 
 function stopSw(){
 
  if(stopTime === false){
      stopTime = true
+     clearTimeout(time)
 
     }
 }
@@ -26,44 +52,24 @@ function resetSw(){
   min = 0;
   hour = 0;
   stopTime = true 
-//   sw.innerHTML = '00:00:00'
+
+  sw.innerHTML = '00:00:00'
 }
 
-function counting(){
- if(stopTime != true){
-        sec++
-    
-
- if(sec == 60){
-     sec = 0
-     min++
-    }
-
- if(min == 60){
-     min = 0;
-     sec = 0;
-     hour++
-    }
-
-    if(sec < 10){sec = '0' + sec}
-    if(min < 10){min = '0' + min}
-    if(hour < 10){hour = '0' + hour}
-
- sw.innerHTML = hour + ":" +  min + ":" +  sec;
-                setTimeout(counting(), 1000);
-  }
-}
  
 // function setSmallNumbers(number){
     // if(number < 10){
         // return '0' + number;
     // }
 // }
+const list = document.getElementById('list')
+
+list.addEventListener('click', (e) => {
+    const el = e.target;
+    console.log(el)
+    if(el.id = 'Stop')stopSw()
+    if(el.id = 'Start')startSw()
+    // if(el.id = 'Reset')resetSw()
+},false)
 
 
-document.addEventListener('click', (e) => {
-    var el = e.target;
-    if(el.id = 'Start'){startSw()}
-    if(el.id = 'Stop'){stopSw()}
-    if(el.id = 'Reset'){resetSw()}
-})
